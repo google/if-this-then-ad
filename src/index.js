438935c514100ecdbb6459a1ddf8d8f0495b43cc
Andriy Kushnarov <andriyku@google.com>
2021-11-23 15:03:53 +0100

    Initial Node.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
