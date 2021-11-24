65502b4637cccc065e93bfdac3dd4007d42e6785
paranerd <paranerd.development@gmail.com>
2021-11-24 11:33:05 +0100

    Added boilerplate

---
+++ server/controllers/some.js
@@ -0,0 +1,7 @@
+function hello(req, res) {
+    res.send('Some Controller');
+}
+
+module.exports = {
+    hello,
+}
--- server/index.js
+++ server/index.js
@@ -3,12 +3,19 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
+
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
   res.send(`Hello ${name}! For Max and Kevin :-)`);
 });
 
-const port = process.env.PORT || 8080;
+
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
 });
