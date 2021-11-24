08b317401c14cbc4e4ade916bcbab240dab75e92
Andriy Kushnarov <andriyku@google.com>
2021-11-24 10:59:38 +0100

    Added changes to test the pipeline

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  res.send(`Hello ${name}! For Max and Kevin :-)`);
 });
 
 const port = process.env.PORT || 8080;
