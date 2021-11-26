bd78d8ef46e3ba85eb8958d40bb5db3b51b900f7
Andriy Kushnarov <andriyku@google.com>
2021-11-26 11:17:30 +0100

    Added pre-commit hook file: code style, build, test

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,20 +1,15 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true,
-    'node': true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
   },
-  'extends': [
-    'google',
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 13,
+  extends: ['google'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
   },
-  'plugins': [
-    '@typescript-eslint',
-  ],
-  'rules': {
-  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
 };
