2c94d9a3819dc58b6b4c2374d6fe59585dc5e726
Andriy Kushnarov <akushnarov@gmail.com>
2022-01-13 12:34:21 +0000

    Added error handling

--- server/controllers/entityProxy.ts
+++ server/controllers/entityProxy.ts
@@ -1,10 +1,20 @@
 import {Request, Response} from 'express';
 
+/**
+ * List of handeled/allowed entity types. See '../models' dir.
+ */
 const allowedEntityTypes: string[] = [
   'user',
   'rule',
 ];
-async function createEntity(entityName: string, id?: string, data?: Object) {
+
+/**
+ * Dynamically creates a model object based on entityName. See '../models' dir.
+ * @param {string} entityName Entity name (e.g. user or rule)
+ * @param {string} id Unique entity ID
+ * @param {Object} data Entity info (in JSON format)
+ */
+async function createModelObject(entityName: string, id?: string, data?: Object) {
   if (! entityName || allowedEntityTypes.indexOf(entityName) < 0) {
     throw new Error(`Entity "${entityName}" is not allowed.`);
   }
@@ -17,7 +27,25 @@ async function createEntity(entityName: string, id?: string, data?: Object) {
 }
 
 /**
- * Save rule to db
+ * Send responses in the standart format
+ * @param {Response} res Response
+ * @param {Object} data Data to return
+ * @param {string} errorMessage Error message if there was an error
+ */
+function sendRes(res: Response, data?: Object, errorMessage?: string) {
+  const resOject: any = {
+    data: data || {},
+  };
+  if (errorMessage) {
+    res.status(400);
+    resOject.message = errorMessage || '';
+  }
+
+  res.send(resOject);
+}
+
+/**
+ * Save entity to db
  * @param {Request} req Request
  * @param {Response}res Response
  */
@@ -25,10 +53,14 @@ async function create(req:Request, res:Response) {
   const id:string = 'UniqueID';
   const configuration:Object = {test: '123'};
 
-  const entity = await createEntity(req.params.entity, id, configuration);
-  await entity.save();
-
-  res.send({success: true});
+  try {
+    const entity = await createModelObject(req.params.entity, id, configuration);
+    await entity.save();
+    sendRes(res);
+  } catch (e: unknown) {
+    const { message } = e as Error;
+    sendRes(res, {}, message);
+  }
 }
 
 /**
@@ -39,10 +71,14 @@ async function create(req:Request, res:Response) {
 async function get(req:Request, res:Response) {
   const id:string = req.params.id;
 
-  const entity = await createEntity(req.params.entity, id);
-  const entityInfo = await entity.get();
-
-  res.send(entityInfo);
+  try {
+    const entity = await createModelObject(req.params.entity, id);
+    const entityInfo = await entity.get();
+    sendRes(res, entityInfo);
+  } catch (e: unknown) {
+    const { message } = e as Error;
+    sendRes(res, {}, message);
+  }
 }
 
 /**
@@ -51,8 +87,13 @@ async function get(req:Request, res:Response) {
  * @param {Response}res Response
  */
 async function list(req:Request, res:Response) {
-  const entity = await createEntity(req.params.entity);
-  res.send(await entity.list());
+  try {
+    const entity = await createModelObject(req.params.entity);
+    sendRes(res, await entity.list());
+  } catch (e: unknown) {
+    const { message } = e as Error;
+    sendRes(res, {}, message);
+  }
 }
 
 module.exports = {
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,16 +1,19 @@
 import {Request, Response, Router} from 'express';
+import app from '../app';
 
 // eslint-disable-next-line new-cap
 const router = Router();
 
-// Controllers
+// Load Controllers
 const entityProxy = require('../controllers/entityProxy');
 
 // Routes:
 //  - API to access the entities from the storage
-router.get('/api/:entity/create', entityProxy.create);
+router.post('/api/:entity/create', entityProxy.create);
 router.get('/api/:entity/list', entityProxy.list);
 router.get('/api/:entity/get/:id', entityProxy.get);
+//router.delete('/api/:entity/delete/:id', entityProxy.delete);
+//router.patch('/api/:entity/update/:id', entityProxy.update);
 
 // Default '/' route
 router.get('/', (req:Request, res:Response) => {
