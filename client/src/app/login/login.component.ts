0f3d5f1b916073f6b05d9b7faca17852056e0d39
paranerd <paranerd.development@gmail.com>
2022-02-17 14:14:35 +0100

    Added more debug code

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from 'src/app/services/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +9,8 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginUrl: string = `${environment.apiUrl}/auth/google`;
+
   constructor(public authService: AuthService) {}
 
   ngOnInit(): void {}
