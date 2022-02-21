2f1a0297c6bd8eaf3a05426576e5760549fdba88
paranerd <paranerd.development@gmail.com>
2022-02-21 17:40:25 +0100

    Fixed formatting

--- client/src/app/interceptors/auth.interceptor.ts
+++ client/src/app/interceptors/auth.interceptor.ts
@@ -4,64 +4,90 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpClient
+  HttpClient,
 } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { catchError, Observable, throwError } from 'rxjs';
-import { User } from '../models/user.model'
+import { User } from '../models/user.model';
 import { Token } from '../interfaces/token';
 import { AuthService } from '../services/auth.service';
 
-
+@Injectable()
 /**
- * Interceptor for HTTP requests, it attaches Authorization 
- * Header and attempts to do a token refresh when  current 
- * access Token expires. 
- * At other layers ensure user is redirected to login 
+ * Interceptor for HTTP requests, it attaches Authorization
+ * Header and attempts to do a token refresh when  current
+ * access Token expires.
+ * At other layers ensure user is redirected to login
  * if Token expired error is thrown.
  */
-@Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  private  MAXRETRIES = 2;
+  private MAXRETRIES = 2;
   private retries = 0;
 
+  /**
+   * Constructor.
+   *
+   * @param {HttpClient} http
+   * @param {AuthService} authService
+   */
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
-  constructor(private http: HttpClient, private authService:AuthService) { }
+  /**
+   * Handle HTTP request.
+   *
+   * @param {HttpRequest<unknown>} request
+   * @param {HttpHandler} next
+   * @returns {Observable<HttpEvent<unknown>>}
+   */
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const accessToken = this.authService.accessToken;
+    const req = request.clone({
+      setHeaders: { Authorization: 'Bearer ' + accessToken },
+    });
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-
-    const accessToken = this.authService.accessToken;     
-    const req = request.clone({ setHeaders: { 'Authorization': 'Bearer ' + accessToken }});
-    
-    return next.handle(req).pipe(catchError((err) => {
-      const codes = new Set([401, 403, 504]);
-      if (codes.has(err.status) && this.retries < this.MAXRETRIES) {
-        this.retries ++; 
-        this.refreshAccessToken().subscribe({
-          next(t) {
-            const retryRequest = req.clone({ setHeaders: { 'Authorization': 'Bearer ' + t.access }});
-            return next.handle(retryRequest);
-          },
-          error(e) { console.error(e) }
-        });
-        // update token on the user 
-        this.refreshAccessToken().subscribe({
-          next: (t) => this.authService.updateToken(t)
-        });
-      }
-      return throwError(() => new Error('Access Token expired: Login again'));
-     }));
+    return next.handle(req).pipe(
+      catchError((err) => {
+        const codes = new Set([401, 403, 504]);
+        if (codes.has(err.status) && this.retries < this.MAXRETRIES) {
+          this.retries++;
+          this.refreshAccessToken().subscribe({
+            next(t) {
+              const retryRequest = req.clone({
+                setHeaders: { Authorization: 'Bearer ' + t.access },
+              });
+              return next.handle(retryRequest);
+            },
+            error(e) {
+              console.error(e);
+            },
+          });
+          // update token on the user
+          this.refreshAccessToken().subscribe({
+            next: (t) => this.authService.updateToken(t),
+          });
+        }
+        return throwError(() => new Error('Access Token expired: Login again'));
+      })
+    );
   }
 
+  /**
+   * Refresh access token.
+   *
+   * @returns {Observable<any>}
+   */
   private refreshAccessToken(): Observable<any> {
-
-    let user: User = this.authService.currentUser!; 
-    const token = this.authService.accessToken; 
+    const user: User = this.authService.currentUser!;
+    const token = this.authService.accessToken;
     const userId = user.id;
     const data = {
       userId,
-      token
-    }
+      token,
+    };
+
     return this.http.post<Token>(`${environment.apiUrl}/auth/refresh`, data);
   }
 }
