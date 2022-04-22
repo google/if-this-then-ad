70b8d43d644ab4b60c95131779ea8b7b0678b684
Max Kimambo <max@kimambo.de>
2022-04-22 10:27:17 +0200

    Review changes

--- server/controllers/account-controller.ts
+++ server/controllers/account-controller.ts
@@ -34,23 +34,6 @@ export const listAccounts = async (req: Request, res: Response) => {
 
 export const create = async (req: Request, res: Response) => {
     // TODO: implement data validation.
-
-    // const user:User = {
-    //     profileId : req.body.profileId,
-    //     displayName : req.body.displayName,
-    //     givenName : req.body.givenName,
-    //     familyName : req.body.familyName,
-    //     gender : req.body.gender,
-    //     email : req.body.email,
-    //     verified : req.body.verified,
-    //     profilePhoto : req.body.profilePhoto,
-    //     locale : req.body.locale,
-    //     authToken : req.body.authToken,
-    //     refreshToken : req.body.refreshToken,
-    //     tokenProvider : req.body.tokenProvider,
-    // };
-
-    // can also be done like this
     const user: User = { ...req.body };
 
     const result = await userRepo.save(user);
@@ -89,9 +72,9 @@ export const updateSettings = async (req: Request, res: Response) => {
 
     } catch (e) {
         log.error(e);
-        return res.status(500).json({ error: 'Error accured while updating user settings.' });
+        return res.status(500).json({ error: 'Error occured while updating user settings' });
     }
-    return res.status(200).json({status: 'ok'});
+    return res.status(200).json({ status: 'ok' });
 };
 
 export const remove = async (req: Request, res: Response) => {
