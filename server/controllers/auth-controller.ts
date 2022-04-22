b35189e42fdd7bbefc249f9334ef041121f204dc
paranerd <paranerd.development@gmail.com>
2022-04-22 13:06:30 +0200

    Auto-formatting

--- server/controllers/auth-controller.ts
+++ server/controllers/auth-controller.ts
@@ -11,31 +11,25 @@
     limitations under the License.
  */
 
-import {Request, Response} from 'express';
-import {log} from '@iftta/util';
-import {refreshToken} from '@iftta/job-runner';
+import { Request, Response } from 'express';
+import { log } from '@iftta/util';
+import { refreshToken } from '@iftta/job-runner';
 
 export const login = (req: Request, res: Response) => {
     // Redirect to authentication
     res.redirect(
-        `/api/auth/google?returnTo=${req.query.returnTo}&clientUrl=${req.query.clientUrl}`
+        `/api/auth/google?returnTo=${req.query.returnTo}&clientUrl=${req.query.clientUrl}`,
     );
 };
 
 export const authDone = (req: Request, res: Response) => {
-    const state = JSON.parse(
-        Buffer.from(req.query.state!.toString(), 'base64').toString()
-    );
+    const state = JSON.parse(Buffer.from(req.query.state!.toString(), 'base64').toString());
     const returnTo = state['returnTo'] || '';
     const clientUrl = state['clientUrl'];
 
     const user = JSON.stringify(req.user) || '';
 
-    res.redirect(
-        `${clientUrl}/logged-in?returnTo=${returnTo}&user=${encodeURIComponent(
-            user
-        )}`
-    );
+    res.redirect(`${clientUrl}/logged-in?returnTo=${returnTo}&user=${encodeURIComponent(user)}`);
 };
 
 export const logout = (req: Request, res: Response) => {
@@ -52,10 +46,10 @@ export const renewToken = (req: Request, res: Response) => {
     const userId = req.body.userId;
     const oldToken = req.body.token;
     refreshToken(userId, oldToken)
-        .then(newToken => {
+        .then((newToken) => {
             res.json(newToken);
         })
-        .catch(reason => {
-            res.status(400).json({status: 'error', message: reason});
+        .catch((reason) => {
+            res.status(400).json({ status: 'error', message: reason });
         });
 };
