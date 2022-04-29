0ec0b38d8edb5aaf211e6b2e233e54124a17987b
paranerd <paranerd.development@gmail.com>
2022-04-29 15:22:13 +0200

    Cannot use pre-built image: 'An image does not exist locally...'

--- setup/post-create.sh
+++ setup/post-create.sh
@@ -2,8 +2,6 @@
 
 echo "Running post create..."
 
-SERVICE_IMAGE='eu.gcr.io/gnd-kimambo/iftta/iftta-service:latest'
-
 # Set Project ID
 echo "Setting Project ID: ${GOOGLE_CLOUD_PROJECT}"
 gcloud config set project ${GOOGLE_CLOUD_PROJECT}
@@ -12,7 +10,7 @@ gcloud config set project ${GOOGLE_CLOUD_PROJECT}
 SESSION_SECRET=$(openssl rand -hex 32)
 
 echo "Deploying Service..."
-gcloud run deploy ${K_SERVICE} --region=${GOOGLE_CLOUD_REGION} --image ${SERVICE_IMAGE} --allow-unauthenticated --no-user-output-enabled --set-env-vars=PROJECT_ID=${GOOGLE_CLOUD_PROJECT},GOOGLE_CLIENT_ID=${GOOGLE_CLIENT_ID},GOOGLE_CLIENT_SECRET=${GOOGLE_CLIENT_SECRET},SESSION_SECRET=${SESSION_SECRET},OAUTH_CALLBACK_URL='',LOG_LEVEL='INFO',NODE_ENV='production',PORT=8080
+gcloud run deploy ${K_SERVICE} --region=${GOOGLE_CLOUD_REGION} --image ${IMAGE_URL} --allow-unauthenticated --no-user-output-enabled --set-env-vars=PROJECT_ID=${GOOGLE_CLOUD_PROJECT},GOOGLE_CLIENT_ID=${GOOGLE_CLIENT_ID},GOOGLE_CLIENT_SECRET=${GOOGLE_CLIENT_SECRET},SESSION_SECRET=${SESSION_SECRET},OAUTH_CALLBACK_URL='',LOG_LEVEL='INFO',NODE_ENV='production',PORT=8080
 
 # Get Cloud Run URL
 CLOUD_RUN_URL=$(gcloud run services describe ${K_SERVICE} --format 'value(status.url)')
