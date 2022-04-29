9f8e9cc3fd014fe9d98bfa42c971fa3e9d84b994
paranerd <paranerd.development@gmail.com>
2022-04-29 16:10:10 +0200

    Adding source to deploy

--- setup/pre-build.sh
+++ setup/pre-build.sh
@@ -10,4 +10,4 @@ gcloud config set project ${GOOGLE_CLOUD_PROJECT}
 SESSION_SECRET=$(openssl rand -hex 32)
 
 echo "Deploying Service..."
-gcloud run deploy ${K_SERVICE} --region=${GOOGLE_CLOUD_REGION} --image ${IMAGE_URL} --allow-unauthenticated --no-user-output-enabled --set-env-vars=PROJECT_ID=${GOOGLE_CLOUD_PROJECT},GOOGLE_CLIENT_ID=${GOOGLE_CLIENT_ID},GOOGLE_CLIENT_SECRET=${GOOGLE_CLIENT_SECRET},SESSION_SECRET=${SESSION_SECRET},OAUTH_CALLBACK_URL='',LOG_LEVEL='INFO',NODE_ENV='production',PORT=8080
+gcloud run deploy ${K_SERVICE} --region=${GOOGLE_CLOUD_REGION} --source . --image ${IMAGE_URL} --allow-unauthenticated --no-user-output-enabled --set-env-vars=PROJECT_ID=${GOOGLE_CLOUD_PROJECT},GOOGLE_CLIENT_ID=${GOOGLE_CLIENT_ID},GOOGLE_CLIENT_SECRET=${GOOGLE_CLIENT_SECRET},SESSION_SECRET=${SESSION_SECRET},OAUTH_CALLBACK_URL='',LOG_LEVEL='INFO',NODE_ENV='production',PORT=8080
