5a910ad92e8eade6ec158f5ae69416535790465e
Max Kimambo <max@kimambo.de>
2022-04-30 19:32:21 +0200

    Google ads UI selector initial work

--- client/src/app/components/googleads-selector/googleads-selector.component.ts
+++ client/src/app/components/googleads-selector/googleads-selector.component.ts
@@ -1,64 +1,71 @@
-import { Component } from '@angular/core';
-import { FlatTreeControl } from '@angular/cdk/tree';
-import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
+/* eslint-disable require-jsdoc */
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { faLightbulb as LightBulbOn, faCirclePause } from '@fortawesome/free-solid-svg-icons';
+// import { faLightbulb as LightBulbOff } from '@fortawesome/free-regular-svg-icons';
 
-/**
- * Account data with nested structure.
- * Each node has a name and an optional list of children.
- */
-interface AccountNode {
+interface AdGroup {
+  customerId: string;
+  campaignId: string;
+  campaignName: string;
+  id: string;
   name: string;
-  children?: AccountNode[];
+  type: string;
+  status: string;
 }
-
-interface ExampleFlatNode {
-  expandable: boolean;
-  name: string;
-  level: number;
-}
-
-const TREE_DATA: AccountNode[] = [
-  {
-    name: 'Google Ads',
-    children: [
-      {name: 'Animals in space', children: [{ name: 'Test' }]},
-      {name: 'My test account', children: [{ name: 'Test' }]}, 
-      {name: 'MCC #1', children: [{ name: 'Test' }]}
-    ],
-  },
-];
 
 @Component({
   selector: 'app-googleads-selector',
   templateUrl: './googleads-selector.component.html',
-  styleUrls: ['./googleads-selector.component.scss']
+  styleUrls: ['./googleads-selector.component.scss'],
 })
-export class GoogleAdsSelectorComponent {
-  private _transformer = (node: AccountNode, level: number) => {
-    return {
-      expandable: !!node.children && node.children.length > 0,
-      name: node.name,
-      level: level,
-    };
-  };
+export class GoogleAdsSelectorComponent implements AfterViewInit {
+  adGroupEnabled = LightBulbOn;
+  adGroupDisabled = faCirclePause;
+  adGroups: AdGroup[] = [];
+  displayedColumns: string[] = ['id', 'name', 'campaignName', 'type', 'status'];
+  selectedRows = new Set<AdGroup>();
+  dataSource = new MatTableDataSource<any>(this.adGroups);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  treeControl = new FlatTreeControl<ExampleFlatNode>(
-    node => node.level,
-    node => node.expandable,
-  );
+  constructor(private http: HttpClient) { }
 
-  treeFlattener = new MatTreeFlattener(
-    this._transformer,
-    node => node.level,
-    node => node.expandable,
-    node => node.children,
-  );
 
-  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
-
-  constructor() {
-    this.dataSource.data = TREE_DATA;
+  // eslint-disable-next-line require-jsdoc
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+  ngOnInit(): void {
+    this.fetchAccountData();
+  }
+
+  selectRow(r: any) {
+    console.log(r);
+    if (this.selectedRows.has(r)) {
+      this.selectedRows.delete(r);
+    } else {
+      this.selectedRows.add(r);
+    }
+  }
+
+  private fetchAccountData() {
+    this.http
+      .get<AdGroup[]>(
+        `${environment.apiUrl}/agents/googleads-agent/list/adgroups`
+      ).pipe(map((res: AdGroup[]) => res))
+      .subscribe((adGroups) => {
+        this.adGroups = adGroups;
+        this.dataSource.data = this.adGroups;
+        this.dataSource.paginator = this.paginator;
+      });
+  }
 }
