fdd5d11ed1c04f61e1a16f6b0fc8eedfab5b9c1d
paranerd <paranerd.development@gmail.com>
2023-03-21 09:12:54 +0100

    Updated configurations and scripts

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,22 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import cleanup from 'rollup-plugin-cleanup';
+import license from 'rollup-plugin-license';
+import { fileURLToPath } from 'url';
 
 const extensions = ['.js'];
 
@@ -18,31 +35,22 @@ const preventTreeShakingPlugin = () => {
 };
 
 export default {
-  input: './build/index.js',
+  input: './build/src/index.js',
   output: {
     dir: 'dist',
     format: 'esm',
-    banner: `/**
-    * @license
-    * Copyright 2023 Google LLC
-    *
-    * Licensed under the Apache License, Version 2.0 (the "License");
-    * you may not use this file except in compliance with the License.
-    * You may obtain a copy of the License at
-    *
-    *      http://www.apache.org/licenses/LICENSE-2.0
-    *
-    * Unless required by applicable law or agreed to in writing, software
-    * distributed under the License is distributed on an "AS IS" BASIS,
-    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    * See the License for the specific language governing permissions and
-    * limitations under the License.
-    */`,
   },
   plugins: [
     preventTreeShakingPlugin(),
     nodeResolve({ extensions }),
     cleanup({ comments: 'none' }),
+    license({
+      banner: {
+        content: {
+          file: fileURLToPath(new URL('license-header.txt', import.meta.url)),
+        },
+      },
+    }),
   ],
   context: 'this',
 };
